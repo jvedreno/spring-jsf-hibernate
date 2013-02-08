b70c02305e52b12f03628cb2b24465d53ed0f7d6
Guillermo Martinez <gmartinezminardi@gmail.com>
2013-02-08 10:03:32 -0300

    ajax

--- src/main/java/com/paradise/app/bean/UserLoginBean.java
+++ src/main/java/com/paradise/app/bean/UserLoginBean.java
@@ -1,33 +1,71 @@
 package com.paradise.app.bean;
 
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.paradise.app.domain.User;
 import com.paradise.app.services.UserService;
 
-@Component(value = "userLoginBean")
-@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
-public class UserLoginBean {
+@ManagedBean(name = "userLoginBean")
+@SessionScoped
+public class UserLoginBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private User loggedUser;
+	private Boolean show = true; 
+	private String text;
 
-	@Autowired
-	private UserService userService;
+//	@Autowired
+//	private UserService userService;
 
 	public User getLoggedUser() {
-		if (loggedUser == null) {
-			String userName = SecurityContextHolder.getContext()
-					.getAuthentication().getName();
-
-			if (userName != null) {
-				loggedUser = userService.findByUsername(userName);
-			}
-		}
+//		if (loggedUser == null) {
+//			String userName = SecurityContextHolder.getContext()
+//					.getAuthentication().getName();
+//
+//			if (userName != null) {
+//				loggedUser = userService.findByUsername(userName);
+//			}
+//		}
 
 		return loggedUser;
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("init");
+		text = "a";
+		
+	}
+	
+	@RequestMapping("/change")
+	public void changeShow() {
+		show = !show;
+	}
+
+	public Boolean getShow() {
+		return show;
+	}
+
+	public void setShow(Boolean show) {
+		this.show = show;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
 	}
 }
--- src/main/java/com/paradise/app/controllers/AdminController.java
+++ src/main/java/com/paradise/app/controllers/AdminController.java
@@ -1,30 +1,32 @@
 package com.paradise.app.controllers;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller(value = "adminController")
 public class AdminController extends AbstractController {
 
-	
 	@RequestMapping("/admin/index.html")
 	public String index() {
 		log.info("/index.htm ");
 		return "admin/index";
 	}
-	
+
 	@RequestMapping(value = "/admin/form.html", method = RequestMethod.POST)
 	public String form(@RequestParam String name, @RequestParam String param) {
 		log.info("/index.htm ");
 		return "/";
 	}
 	
-	public Boolean pepe() {
-		return false;
-		
+	@RequestMapping(value = "/time.html", method = RequestMethod.GET)
+	public @ResponseBody String getTime(@RequestParam String name) {
+		String result = "Time for " + name + " is " + new Date().toString();
+		return result;
 	}
-	
+
 }
